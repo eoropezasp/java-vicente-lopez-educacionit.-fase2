0c19600a5640bf2aad38a7f3156eb618c5a14dbb
emmanuel <eoropezasp@gmail.com>
2023-09-16 20:58:44 -0300

    borrado

--- src/ar/com/educacionit/universidad/oop/interfaces/IIDioma.java
+++
@@ -1,9 +0,0 @@
-package ar.com.educacionit.universidad.oop.interfaces;
-
-public interface IIDioma {
-
-	 // las clases que implementes esta interface deben completar el metodo
-
-	void decir(String algo);
-	
-}
